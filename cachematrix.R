dd6591c0d0b2a8b067167dd1f6edff1d2a873293
vlearn <vijayag@maqsoftware.com>
2014-08-18 20:22:28 +0530

    Added comments to explain the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,52 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to cache the inverse of a matix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a matrix object that 
+## can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
         
+        ##Initialize the inverse           
         i <- NULL
         
+        ## Method to set the matrix
         set <- function (matrix)
         {
                 m <<- matrix
                 i <<- NULL
         }
         
+        ## Method to get the matrix
         get <- function(matrix){
                 m
         }
         
+        ## Method to set the inverse of the matrix
         setInverse <- function(inverse){
                 i <<- inverse
         }
         
+        ## Method to get the inverse of the matrix
         getInverse <- function(){ 
                 i 
         }
         
+        ## Returns a list of methods
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the matrix returned
+## by "makeCacheMatrix" function. If the inverse is already
+## calculated, then the below function will return the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         
+        ## Return a matrix that is the inverse of 'x'
         m <- x$getInverse()
         
+        ## Return the inverse, if its already set
         if ( !is.null(m) ){
                 message("getting cached data")
                 return(m)
         }
-
+        
+        ## Get the matrix from our object
         data <- x$get()
 
+        ## Calculate the inverse using matrix multiplication
         m <- solve(data) %*% data
 
+        ## Set the inverse to the object        
         x$setInverse(m)
-
+        
+        ## Return the matrix
         m
 }
