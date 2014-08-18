a77d8b52cc496ed33cfca3fa66a57d32a045bbf9
vlearn <vijayag@maqsoftware.com>
2014-08-18 20:02:48 +0530

    Checking in makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        i <- NULL
+        
+        set <- function (matrix)
+        {
+                m <<- matrix
+                i <<- NULL
+        }
+        
+        get <- function(matrix){
+                m
+        }
+        
+        setInverse <- function(inverse){
+                i <<- inverse
+        }
+        
+        getInverse <- function(){ 
+                i 
+        }
+        
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
